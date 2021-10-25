2020539caf0a04e8c5f889c6f476dc0d0a6a9d71
syrine868 <syrine868@gmail.com>
2021-10-25 15:29:09 +0100

    update cross origin

--- src/main/java/com/zuul/demo/ZuulRecrutementServerProjectApplication.java
+++ src/main/java/com/zuul/demo/ZuulRecrutementServerProjectApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @EnableDiscoveryClient
 @EnableZuulProxy
 @SpringBootApplication
+@CrossOrigin(origins = "http://localhost:4200")
 public class ZuulRecrutementServerProjectApplication {
 
 	public static void main(String[] args) {
